c716f836f2cc103463a1e790bc6b142623eeef59
lisa <1085983505@qq.com>
2017-06-11 19:31:14 +0800

    Initial Commit

---
+++ PrintDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PrintDemo
+//
+//  Created by lisa on 2017/6/11.
+//  Copyright © 2017年 lisa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PrintDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PrintDemo
+//
+//  Created by lisa on 2017/6/11.
+//  Copyright © 2017年 lisa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
